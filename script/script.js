ecf9a59ba5372a63074e24d5d7abba307fcebea7
Matias Belvedere <matiasbelvedere@hotmail.com>
2022-10-21 17:06:09 -0300

    testing1

--- script/script.js
+++ script/script.js
@@ -7,41 +7,48 @@ const welcTwo = document.getElementById("welc2");
 const welcTwoP = document.querySelector("#welc2 p")
 let banner = document.querySelector(".banner")
 
+const drinksPhoto = document.querySelectorAll(".drinksPhoto");
 
-if (window.matchMedia("(min-width: 1023px)").matches) {
-    setTimeout(() => {
-        //desktop
-        welc.style.transition="5s ease-in-out";
-        welc.style.transform="translateY(40px)";
-        welc.style.boxShadow="0px 5px 10px white";
+// drinksPhoto.forEach(e => {
+//     e.addEventListener("mouseenter", () => {
+//         e.style.transform = "scale(1)"
+//     })
+// })
+
+// if (window.matchMedia("(min-width: 1023px)").matches) {
+//     setTimeout(() => {
+//         //desktop
+//         welc.style.transition="5s ease-in-out";
+//         welc.style.transform="translateY(40px)";
+//         welc.style.boxShadow="0px 5px 10px white";
     
-        welcP.style.transform="translateX(200px)"
-        welcP.style.transition="5s ease-in-out";
-        welcP.style.opacity="1"
+//         welcP.style.transform="translateX(200px)"
+//         welcP.style.transition="5s ease-in-out";
+//         welcP.style.opacity="1"
         
     
-        welcTwo.style.transition="5s ease-in-out";
-        welcTwo.style.transform="translateY(-40px)";
-        welcTwo.style.boxShadow="0px -5px 10px white";
+//         welcTwo.style.transition="5s ease-in-out";
+//         welcTwo.style.transform="translateY(-40px)";
+//         welcTwo.style.boxShadow="0px -5px 10px white";
     
-        welcTwoP.style.transform="translateX(-200px)"
-        welcTwoP.style.transition="5s ease-in-out";
-    }, 1000);
-} else {
-    setTimeout(() => {
-        //mobile
-    welc.style.transition="5s ease-in-out";
-    welc.style.transform="translateY(45px)";
-    welc.style.boxShadow="0px 5px 10px white";
+//         welcTwoP.style.transform="translateX(-200px)"
+//         welcTwoP.style.transition="5s ease-in-out";
+//     }, 1000);
+// } else {
+//     setTimeout(() => {
+//         //mobile
+//     welc.style.transition="5s ease-in-out";
+//     welc.style.transform="translateY(45px)";
+//     welc.style.boxShadow="0px 5px 10px white";
 
-    welcP.style.transition="5s ease-in-out"
-    welcP.style.opacity="1"
+//     welcP.style.transition="5s ease-in-out"
+//     welcP.style.opacity="1"
 
-    welcTwo.style.transition="5s ease-in-out";
-    welcTwo.style.transform="translateY(-45px)";
-    welcTwo.style.boxShadow="0px -5px 10px white";
-    }, 1000);
-}
+//     welcTwo.style.transition="5s ease-in-out";
+//     welcTwo.style.transform="translateY(-45px)";
+//     welcTwo.style.boxShadow="0px -5px 10px white";
+//     }, 1000);
+// }
 
 
 
